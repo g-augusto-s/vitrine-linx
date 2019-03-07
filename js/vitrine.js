8e69bd56ede75b3f934e9488fdbb3303566cda59
Guilherme <gaugusto.silva94@gmail.com>
2019-03-07 18:47:01 -0300

    Update iteration

--- js/vitrine.js
+++ js/vitrine.js
@@ -1,39 +1,90 @@
 const X = response => {
 
-    this.widget = null;
-    this.reference = null;
-    this.recommendation = null;
+    // ------------ Global variables ------------
+    let widget = null;
+    let reference = null;
+    let recommendations = null;
 
-    for (const key in response.data) {
-        if (response.data.hasOwnProperty(key)) {
-            const obj = response.data[key];
-            switch(key){
-                case "widget":
-                    this.widget = obj;
-                    break;
-                
-                case "reference":
-                    this.reference = obj;
-                    break;
-                
-                case "recommendation":
-                    this.recommendation = obj;
-                    break;
+    // ---------- Functions ------------
 
-                default:
-                    console.log("Erro");
-                    break;
-                
+    // Get data from JSONP
+    getData = () => {
+        for (const key in response.data) {
+            if (response.data.hasOwnProperty(key)) {
+                const obj = response.data[key];
+                switch(key){
+                    case "widget":
+                        widget = obj;
+                        break;
+                    
+                    case "reference":
+                        reference = obj;
+                        break;
+                    
+                    case "recommendation":
+                        recommendations = obj;
+                        break;
+    
+                    default:
+                        console.log("Erro");
+                        break;
+                }
             }
         }
     }
-    console.log("variaveis de estado: ");
-    console.log("this.widget: ", this.widget);
-    console.log("this.reference: ", this.reference);
-    console.log("this.recommendation: ", this.recommendation);
 
-    this.recommendation.map(recomendations => {
-        // console.log("recomendations: ", recomendations);
-    });
+    // Render product reference
+    renderProduct = (ref) => {
+        return `
+            <div class="col-2" >
+                <a class="" href="${ref.item.detailUrl}" >
+                    <img src="${ref.item.imageName}" class=""/>
+                    <div class="row">
+                        <p class="" >${ref.item.name}</p>
+                        ${ref.item.oldPrice ? `<p>De: ${ref.item.oldPrice}</p>` : ''}
+                        <div class="promocao" >
+                            <h5>Por: <strong>${ref.item.price}</strong></h5>
+                            <p>${ref.item.productInfo.paymentConditions}</p>
+                            <p>sem juros</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        `;
+    }
 
+    // Render product recommendations
+    renderRecomendations = (rec) => {
+        return `
+        ${rec.map(recommendation => 
+            `<div class="col-2" >
+                <a class="" href="${recommendation.detailUrl}" >
+                    <img src="${recommendation.imageName}" class=""/>
+                    <div class="row">
+                        <p class="" >${recommendation.name}</p>
+                        ${recommendation.oldPrice ? `<p>De: ${recommendation.oldPrice}</p>` : ''}
+                        <div class="promocao" >
+                            <h5>Por: <strong>${recommendation.price}</strong></h5>
+                            <p>${recommendation.productInfo.paymentConditions}</p>
+                            <p>sem juros</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            `
+        ).join('')}
+        `;
+    }
+    
+
+    // Get the container element and update the innerHTML of him
+    updateTemplate = (productContainer, ref, rec) => {
+        let containerElement = document.getElementById(productContainer);
+        containerElement.innerHTML = renderProduct(ref) + renderRecomendations(rec);
+    }
+
+    this.getData();
+    this.updateTemplate("product-container", reference, recommendations);
+
+    
 }
